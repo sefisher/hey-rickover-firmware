c6218cd84602b56234575ee38f6c87e961152dea
Stephen Fisher <stephen.e.fisher@gmail.com>
2021-08-15 12:05:47 -0400

    update

--- src/config.h
+++ src/config.h
@@ -1,19 +1,19 @@
 
-//this file contains the senstive parameters below.  you can  add your own secrets.h 
+//this file contains the senstive parameters below.  you can  add your own secrets.h
 //or comment this out and enter them directly below.
 #include "secrets.h"
 
 // WiFi credentials
 #ifndef WIFI_SSID
-    #define WIFI_SSID "PUT YOUR SSID"
+#define WIFI_SSID "PUT YOUR SSID"
 #endif
 #ifndef WIFI_PSWD
-    #define WIFI_PSWD "PUT YOUR PASS"
+#define WIFI_PSWD "PUT YOUR PASS"
 #endif
 
 // Command recognition settings
 #ifndef COMMAND_RECOGNITION_ACCESS_KEY
-    #define COMMAND_RECOGNITION_ACCESS_KEY "PUT YOUR ACCESS KEY"
+#define COMMAND_RECOGNITION_ACCESS_KEY "PUT YOUR ACCESS KEY"
 #endif
 
 // are you using an I2S microphone - comment this out if you want to use an analog mic and ADC input
@@ -24,9 +24,9 @@
 // Which channel is the I2S microphone on? I2S_CHANNEL_FMT_ONLY_LEFT or I2S_CHANNEL_FMT_ONLY_RIGHT
 #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
 // #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_RIGHT
-#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_26
-#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_22
-#define I2S_MIC_SERIAL_DATA GPIO_NUM_21
+#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_26     //SCLK or BCLK
+#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_22 //LRCL or WS
+#define I2S_MIC_SERIAL_DATA GPIO_NUM_21      //SD or DOUT
 
 // Analog Microphone Settings - ADC1_CHANNEL_7 is GPIO35
 #define ADC_MIC_CHANNEL ADC1_CHANNEL_7
